4169bcc995ef3258f78376200a8ea5d687c4c981
AdamG63 <adamghumra43@gmail.com>
2022-05-17 10:23:41 +0100

    This commit will return the articles and handle the errors

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const { getTopics } = require('./controller/topics.contoller');
+const { getArticleById } = require('./controller/articles.controller');
 const app = express();
 
 app.get('/api/topics', getTopics);
 
+app.get('/api/articles/:article_id', getArticleById)
+
+app.use((err, req, res, next) => {
+   if(err.code === '22P02'){
+       res.status(400).send({message: 'Bad request'})
+   } else { 
+       next(err) }
+})
+app.use((err, req, res, next) => {
+    if(err.status === 404){
+        res.status(404).send({msg: 'Not found'})
+    } else {
+        next(err)
+    }
+})
 
 app.all('/*', (req, res) => {
     res.status(404).send({msg: 'Not found'})
 })
 app.use((err, req, res, next) => {
+    console.log(err)
     res.status(500).send('internal server error');
 });
-
-
 
 module.exports = app;
